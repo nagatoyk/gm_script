41f21f7d7cd8d7f621cc725479a87c5250b31a67
镜花水月 <393117228@qq.com>
2015-06-20 14:31:49 +0800

    修改
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- tudou.user.js
+++ tudou.user.js
@@ -3,9 +3,10 @@
 // @namespace   https://yuki-yukimax.rhcloud.com
 // @description 把烦人的土豆侧栏关掉
 // @match       http://*.tudou.com/*
-// @updateURL 	https://github.com/nagatoyk/gm_script/raw/27dc7016c1ee457cc69c7858af623f3a8ca2c219/tudou.user.js
-// @downloadURL https://github.com/nagatoyk/gm_script/raw/27dc7016c1ee457cc69c7858af623f3a8ca2c219/tudou.user.js
-// @version     0.3
+// @require     http://yukimax.sinaapp.com/f/jQuery.ScrollTo.js
+// @updateURL 	https://github.com/nagatoyk/gm_script/raw/master/tudou.user.js
+// @downloadURL https://github.com/nagatoyk/gm_script/raw/master/tudou.user.js
+// @version     0.4
 // @author      小熊
 // @grant       none
 // ==/UserScript==
@@ -20,6 +21,7 @@ $(document).ready(function() {
 			switch (location.pathname.substring(1, 9)) {
 				case 'albumpaly':
 				case 'listplay':
+					$.scrollTo('#player', 500);
 					console.log(location.pathname.substring(1, 9));
 					break;
 			}
