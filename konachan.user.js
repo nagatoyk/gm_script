27dc7016c1ee457cc69c7858af623f3a8ca2c219
镜花水月 <393117228@qq.com>
2015-06-20 14:05:09 +0800

    添加 (konachan.user.js)
    Signed-off-by: 镜花水月 <393117228@qq.com>

---
+++ konachan.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        konachan
+// @namespace   https://yuki-yukimax.rhcloud.com
+// @description 抓图
+// @match       http://konachan.com/*
+// @require     http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+var jQ172 = $.noConflict(true);
+(function($) {
+	console.log($.fn.jquery);
+	switch (location.pathname.substring(1, 10)) {
+		case 'post':
+			console.log(location);
+			$('li .thumb').each(function() {
+				console.log($(this));
+				$(this).attr('href', '/post/show/' + ($(this).attr('href')).match('\\d{1,}')[0]);
+				$(this).attr('target', '_konachan');
+			});
+			break;
+		case 'post/show':
+			pid = location.pathname.match('\\d{1,}')[0];
+			source = location.href.match('http\:\/\/konachan\.com\/post\/show\/\\d{1,}')[0];
+			imgurl = $('#highres').attr('href');
+			console.log(pid, source, imgurl);
+			$.post('http://yukimax.sinaapp.com/p/saveImg.php?pid=' + pid, {imgOpt:{url:imgurl,source:source}}, function(r) {
+				if (!r.id || r.pid == 0 || r.error) {
+					$.post('http://yukimax.sinaapp.com/p/saveImg.php?pid=' + pid, {imgOpt:{url:imgurl,source:source}}, function(r) {
+						console.log(r);
+					}, 'json');
+				} else {
+					console.log(r);
+				}
+			}, 'json');
+			break;
+	}
+}) (jQ172);
