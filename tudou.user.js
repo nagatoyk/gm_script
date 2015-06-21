abdb54e0cdb728d4b726fc01efb2fc81dc633f84
镜花水月 <393117228@qq.com>
2015-06-21 11:38:22 +0800

    修改
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- tudou.user.js
+++ tudou.user.js
@@ -13,20 +13,22 @@
 // ==/UserScript==
 var jQ172 = $.noConflict(true);
 (function($) {
-	switch (location.hostname) {
-		case 'cartoon.tudou.com':
-			if (!$('body').attr('class')) {
-				$('body').addClass('g-sidebar-hide').addClass('w1216');
-			}
-			break;
-		case 'www.tudou.com':
-			switch (location.pathname.substring(1, 9)) {
-				case 'albumpaly':
-				case 'listplay':
-					$.scrollTo('#player', 500);
-					console.log(location.pathname.substring(1, 9));
-					break;
-			}
-			break;
-	}
+	$(document).ready(function() {
+		switch (location.hostname) {
+			case 'cartoon.tudou.com':
+				if (!$('body').attr('class')) {
+					$('body').addClass('g-sidebar-hide').addClass('w1216');
+				}
+				break;
+			case 'www.tudou.com':
+				switch (location.pathname.substring(1, 9)) {
+					case 'albumpaly':
+					case 'listplay':
+						$.scrollTo('#player', 500);
+						console.log(location.pathname.substring(1, 9));
+						break;
+				}
+				break;
+		}
+	});
 }) (jQ172);
