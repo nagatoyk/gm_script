0c0cd0e4905a3996f221b6209ef93eb912782715
镜花水月 <393117228@qq.com>
2015-06-21 12:34:58 +0800

    修改 tudou.user.js
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- tudou.user.js
+++ tudou.user.js
@@ -7,26 +7,28 @@
 // @require     http://yukimax.sinaapp.com/f/jQuery.ScrollTo.js
 // @updateURL 	https://github.com/nagatoyk/gm_script/raw/master/tudou.user.js
 // @downloadURL https://github.com/nagatoyk/gm_script/raw/master/tudou.user.js
-// @version     0.6
+// @version     0.7
 // @author      小熊
 // @grant       none
 // ==/UserScript==
 var jQ172 = $.noConflict(true);
 (function($) {
-	switch (location.hostname) {
-		case 'cartoon.tudou.com':
-			if (!$('body').attr('class')) {
-				$('body').addClass('g-sidebar-hide').addClass('w1216');
-			}
-			break;
-		case 'www.tudou.com':
-			switch (location.pathname.substring(1, 9)) {
-				case 'albumpaly':
-				case 'listplay':
-					// $.scrollTo('#player', 500);
-					console.log(location.pathname.substring(1, 9));
-					break;
-			}
-			break;
-	}
+	$(document).ready(function() {
+		switch (location.hostname) {
+			case 'cartoon.tudou.com':
+				if (!$('body').attr('class')) {
+					$('body').addClass('g-sidebar-hide').addClass('w1216');
+				}
+				break;
+			case 'www.tudou.com':
+				switch (location.pathname.substring(1, 9)) {
+					case 'albumpaly':
+					case 'listplay':
+						// $.scrollTo('#player', 500);
+						console.log(location.pathname.substring(1, 9));
+						break;
+				}
+				break;
+		}
+	});
 }) (jQ172);
