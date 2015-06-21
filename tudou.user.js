9f953e26554f13c0d85199d8e2465ee1835a9016
镜花水月 <393117228@qq.com>
2015-06-21 12:52:02 +0800

    修改 tudou.user.js
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- tudou.user.js
+++ tudou.user.js
@@ -4,7 +4,6 @@
 // @description 把烦人的土豆侧栏关掉
 // @match       http://*.tudou.com/*
 // @require     http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js
-// @require     http://yukimax.sinaapp.com/f/jQuery.ScrollTo.js
 // @updateURL 	https://github.com/nagatoyk/gm_script/raw/master/tudou.user.js
 // @downloadURL https://github.com/nagatoyk/gm_script/raw/master/tudou.user.js
 // @version     0.7
@@ -13,22 +12,24 @@
 // ==/UserScript==
 var jQ172 = $.noConflict(true);
 (function($) {
-	$(document).ready(function() {
-		switch (location.hostname) {
-			case 'cartoon.tudou.com':
-				if (!$('body').attr('class')) {
-					$('body').addClass('g-sidebar-hide').addClass('w1216');
-				}
-				break;
-			case 'www.tudou.com':
-				switch (location.pathname.substring(1, 9)) {
-					case 'albumpaly':
-					case 'listplay':
-						// $.scrollTo('#player', 500);
-						console.log(location.pathname.substring(1, 9));
-						break;
-				}
-				break;
-		}
-	});
+	switch (location.hostname) {
+		case 'cartoon.tudou.com':
+			if (!$('body').attr('class')) {
+				$('body').addClass('g-sidebar-hide').addClass('w1216');
+			}
+			console.log(location.hostname);
+			break;
+		case 'www.tudou.com':
+			switch (location.pathname.substring(1, 9)) {
+				case 'albumpaly':
+				case 'listplay':
+					$('html,body').animate({
+						scrollTop: $('#player').offset().top
+					}, 'slow');
+					console.log(location.pathname.substring(1, 9));
+					break;
+			}
+			console.log(location.hostname, $('#player').offset().top);
+			break;
+	}
 }) (jQ172);
