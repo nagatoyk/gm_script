04cf7c4520e236f2f3b8a1e51b7d10e72d957996
镜花水月 <393117228@qq.com>
2015-06-26 19:52:51 +0800

    修改 jlist.user.js
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- jlist.user.js
+++ jlist.user.js
@@ -2,7 +2,7 @@
 // @name        Jlist
 // @namespace   https://yuki-yukimax.rhcloud.com
 // @description 抓图
-// @match       http://jlist.com/product/*
+// @match       http://www.jlist.com/product/*
 // @require     http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js
 // @updateURL   https://github.com/nagatoyk/gm_script/raw/master/jlist.user.js
 // @downloadURL https://github.com/nagatoyk/gm_script/raw/master/jlist.user.js
@@ -13,4 +13,14 @@
 var jQ172 = $.noConflict(true);
 (function($) {
 	console.log($.fn.jquery);
+	baseImg = $('.base-image img').attr('src');
+	fancyList = $('#productgallery >.fancy');
+	console.log(baseImg);
+	console.log(fancyList);
+	m = [];
+	$('#productgallery .fancy').each(function(i, e) {
+		console.log(i, e);
+		m.push(i: {'link': e.href});
+	});
+	console.log(m);
 }) (jQ172);
